150e3ad27475bf24f934f6c666abde424f4ddcb9
Kelly Taylor <taylok10@aston.ac.uk>
2019-08-08 18:10:14 +0100

    Added comments to LinearNode and formatted

--- src/model/LinearNode.java
+++ src/model/LinearNode.java
@@ -10,30 +10,50 @@ package model;
 public class LinearNode<T> {
 	private T element;
 	private LinearNode<T> next;
-	
-	public LinearNode(T element)
-	{
+
+	/**
+	 * Creates a new LinearNode
+	 * 
+	 * @param The element of the LinearNode
+	 */
+	public LinearNode(T element) {
 		next = null;
 		this.element = element;
 	}
-	
-	public LinearNode<T> getNext()
-	{
+
+	/**
+	 * Gets the node 'following' this node
+	 * 
+	 * @return The node 'following' this node
+	 */
+	public LinearNode<T> getNext() {
 		return next;
 	}
-	
-	public void setNext(LinearNode<T> node)
-	{
+
+	/**
+	 * Sets the node 'following' this node
+	 * 
+	 * @param node The node 'following' this node
+	 */
+	public void setNext(LinearNode<T> node) {
 		next = node;
 	}
-	
-	public T getElement()
-	{
+
+	/**
+	 * Gets the element of the LinearNode
+	 * 
+	 * @return The element of the LinearNode
+	 */
+	public T getElement() {
 		return element;
 	}
-	
-	public void setElement(T element)
-	{
+
+	/**
+	 * Sets the element of the LinearNode
+	 * 
+	 * @param element The element to set against the LinearNode
+	 */
+	public void setElement(T element) {
 		this.element = element;
 	}
 }
