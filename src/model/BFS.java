163ffd89f205b30e7beca3b7d486b63857bb6367
Kelly Taylor <taylok10@aston.ac.uk>
2019-08-08 18:02:44 +0100

    Added comments to BFS and formatted

--- src/model/BFS.java
+++ src/model/BFS.java
@@ -13,62 +13,83 @@ import java.util.Queue;
 public class BFS<T extends GridLocation> implements PathFinder<T> {
 	private T[][] grid;
 	private Queue<LinearNode<T>> tree;
-	
+
+	/**
+	 * Will create a new BFS object
+	 * 
+	 * @param grid The grid to use for path finding
+	 */
 	public BFS(T[][] grid) {
 		this.grid = grid;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see model.PathFinder#findPath(model.Cell, model.Cell)
 	 */
 	@Override
 	public LinearNode<T> findPath(T startLocation, T targetLocation) {
-        tree = new LinkedList<LinearNode<T>>();
-        //Start at the target location so we can navigate the LinearNodes from start to finish
-        LinearNode<T> initialNode = new LinearNode<T>(targetLocation);
-        tree.add(initialNode);
-        
-        LinearNode<T> path = searchNodes(startLocation);
-        return path;
+		tree = new LinkedList<LinearNode<T>>();
+		// Start at the target location so we can navigate the LinearNodes from start to
+		// finish
+		LinearNode<T> initialNode = new LinearNode<T>(targetLocation);
+		tree.add(initialNode);
+		LinearNode<T> path = searchNodes(startLocation);
+		return path;
 	}
-	
-	private void tryMove(LinearNode<T> node, int x, int y) {
+
+	/**
+	 * Will attempt to move to the given x and y position in the grid
+	 * 
+	 * @param node The current position
+	 * @param x    The x coordinate to move to
+	 * @param y    The y coordinate to move to
+	 * @return If the move was successful
+	 */
+	private boolean tryMove(LinearNode<T> node, int x, int y) {
 		boolean yIsValid = y >= 0 && y < grid.length;
-		if(yIsValid) {
+		if (yIsValid) {
 			boolean xIsValid = x >= 0 && x < grid[y].length;
-			if(xIsValid) {
-	        	// Obstacle check can occur here if needed
-	        	LinearNode<T> element = new LinearNode<T>(grid[y][x]);
-	        	element.setNext(node);
-	        	tree.add(element);
+			if (xIsValid) {
+				// Obstacle check can occur here if needed
+				LinearNode<T> element = new LinearNode<T>(grid[y][x]);
+				element.setNext(node);
+				tree.add(element);
+				return true;
 			}
 		}
+		return false;
 	}
 
-    private LinearNode<T> searchNodes(T endLocation)
-    {
-        while (!tree.isEmpty())
-        {
-        	LinearNode<T> node = tree.remove();
-        	if(node.getElement() == endLocation) {
-        		// We've reached the target. Send this path back.
-        		return node;
-        	}
-            int x = node.getElement().getColumn();
-            int y = node.getElement().getRow();
+	/**
+	 * Finds a path Searches the grid for a path to the provided location
+	 * 
+	 * @param endLocation Where do we need to end
+	 * @return the found path, null if none found
+	 */
+	private LinearNode<T> searchNodes(T endLocation) {
+		while (!tree.isEmpty()) {
+			LinearNode<T> node = tree.remove();
+			if (node.getElement() == endLocation) {
+				// We've reached the target. Send this path back.
+				return node;
+			}
+			int x = node.getElement().getColumn();
+			int y = node.getElement().getRow();
 
-            //Try to move left
-            tryMove(node, x - 1, y);
+			// Try to move left
+			tryMove(node, x - 1, y);
 
-            //Try to move up
-            tryMove(node, x, y - 1);
+			// Try to move up
+			tryMove(node, x, y - 1);
 
-            //Try to move right
-            tryMove(node, x + 1, y);
+			// Try to move right
+			tryMove(node, x + 1, y);
 
-            //Try to move down
-            tryMove(node, x, y + 1);
-        }
-        return null;
-    }
+			// Try to move down
+			tryMove(node, x, y + 1);
+		}
+		return null;
+	}
 }
