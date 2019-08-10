2d1421b3ad40932d7fc25ef12130a3a95fa06cee
Joseph Sheargold <sheargoldjoseph@gmail.com>
2019-08-10 14:55:43 +0100

    Revert "Initial *.sim file reading with MainTest.java"
    This reverts commit 0bc92abba3e9ef46f7b18f033fc63b9d30495f10.

--- src/controller/WarehouseController.java
+++ src/controller/WarehouseController.java
@@ -1,38 +1,28 @@
 package controller;
 
-import java.io.File;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
-import javafx.stage.FileChooser;
-import javafx.stage.FileChooser.ExtensionFilter;
 import model.Cell;
 import model.ChargingPod;
 import model.Floor;
 import model.PackingStation;
 import model.Robot;
 import model.StorageShelf;
-import model.WarehouseSimulation;
-import view.MainTest;
 
 public class WarehouseController {
 	@FXML Slider sliderWidth, sliderHeight;
 	@FXML Floor floor;
 	@FXML Button buttonAddRobot, buttonRemoveRobot, buttonAddShelf, buttonRemoveShelf, buttonAddPacker, buttonRemovePacker;
-	@FXML MenuItem menuItemImport;
 	
-	private MainTest mt;
 	private static Cell userCell;
 	private static int userX, userY;
 	
-	public WarehouseController(MainTest mt) {
-		this.mt = mt;
+	public WarehouseController() {
 		userCell = null;
 		userX = 0;
 		userY = 0;
@@ -131,17 +121,6 @@ public class WarehouseController {
 		userX = userCell.getColumn();
 		userY = userCell.getRow();
 		System.out.println(userCell.getActorsDesc());
-	}
-	
-	public void menuItemImport() {
-		FileChooser fc = new FileChooser();
-		fc.getExtensionFilters().addAll(new ExtensionFilter("Simulation Files","*.sim"));
-		File selectedFile = fc.showOpenDialog(null);
-		if (selectedFile != null) {
-			mt.loadImport(selectedFile);
-		} else {
-			System.out.println("Invalid File Selected");
-		}
 	}
 	
 	
--- src/model/Floor.java
+++ src/model/Floor.java
@@ -1,6 +1,5 @@
 package model;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
