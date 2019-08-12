6e6e1dcdf6be384c8176c298b2b60d4e03700a30
Kelly Taylor <taylok10@aston.ac.uk>
2019-08-12 18:27:36 +0100

    Unit testing and tidying up Order

--- src-test/model/OrderTest.java
+++ src-test/model/OrderTest.java
@@ -3,7 +3,10 @@
  */
 package model;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -13,9 +16,107 @@ import org.junit.Test;
  */
 public class OrderTest {
 	@Test
-	public void testGetShelves() {
-		//StorageShelf[] shelves = { new StorageShelf(0, 0), new StorageShelf(0, 0), new StorageShelf(0, 0) };
-		//Order order = new Order(shelves);
-		//assertArrayEquals(shelves, order.getShelves());
+	public void testAddItemToEmptyList() {
+		Order emptyOrder = new Order(1, new StorageShelf[] {});
+		StorageShelf shelf = new StorageShelf(0, 0);
+		emptyOrder.addItem(shelf);
+		assertEquals(1, emptyOrder.outstandingItemsSize());
+		assertEquals(shelf, emptyOrder.getNextItem().getLocation());
+	}
+
+	@Test
+	public void testAddDuplicateItem() {
+		StorageShelf shelf = new StorageShelf(0, 0);
+		Order order = new Order(1, new StorageShelf[] { shelf });
+		order.addItem(shelf);
+		assertEquals(1, order.outstandingItemsSize());
+		assertEquals(2, order.getNextItem().getQty());
+	}
+
+	@Test
+	public void testGetTicksToPack() {
+		Order order = new Order(3, new StorageShelf[] {});
+		assertEquals(3, order.getTicksToPack());
+	}
+
+	@Test
+	public void testDecrementTicksToPack() {
+		Order order = new Order(3, new StorageShelf[] {});
+		assertTrue(order.decrementTicksToPack());
+		assertEquals(2, order.getTicksToPack());
+		assertTrue(order.decrementTicksToPack());
+		assertEquals(1, order.getTicksToPack());
+		assertTrue(order.decrementTicksToPack());
+		assertEquals(0, order.getTicksToPack());
+	}
+
+	@Test
+	public void testDecrementTicksToPackBelowZero() {
+		Order order = new Order(0, new StorageShelf[] {});
+		assertFalse(order.decrementTicksToPack());
+		assertEquals(0, order.getTicksToPack());
+	}
+
+	@Test
+	public void testGetTimeProcessing() {
+		Order order = new Order(0, new StorageShelf[] {});
+		assertEquals(0, order.getTimeProcessing());
+	}
+
+	@Test
+	public void testIncrementTimeProcessing() {
+		Order order = new Order(0, new StorageShelf[] {});
+		order.incrementTimeProcessing();
+		assertEquals(1, order.getTimeProcessing());
+		order.incrementTimeProcessing();
+		assertEquals(2, order.getTimeProcessing());
+	}
+
+	@Test
+	public void testGetNextItem() {
+		StorageShelf shelf = new StorageShelf(0, 0);
+		Order order = new Order(0, new StorageShelf[] { shelf });
+		assertEquals(shelf, order.getNextItem().getLocation());
+	}
+
+	@Test
+	public void testGetNextItemWhenNoMoreItems() {
+		Order order = new Order(0, new StorageShelf[] {});
+		assertNull(order.getNextItem());
+	}
+
+	@Test
+	public void testIsComplete() {
+		Order order = new Order(0, new StorageShelf[] {});
+		assertTrue(order.isComplete());
+		order.addItem(new StorageShelf(0, 0));
+		assertFalse(order.isComplete());
+	}
+
+	@Test
+	public void testIsPacked() {
+		Order order = new Order(1, new StorageShelf[] {});
+		assertFalse(order.isPacked());
+		order.decrementTicksToPack();
+		assertTrue(order.isPacked());
+	}
+
+	@Test
+	public void testProcessOrder() {
+		StorageShelf shelf = new StorageShelf(0, 0);
+		Order order = new Order(1, new StorageShelf[] { shelf });
+		assertTrue(order.processItem(order.getNextItem()));
+		assertEquals(0, order.outstandingItemsSize());
+		assertEquals(1, order.processedItemsSize());
+	}
+
+	@Test
+	public void testProcessOrderInvalidOrder() {
+		StorageShelf shelf = new StorageShelf(0, 0);
+		StorageShelf shelfTwo = new StorageShelf(0, 1);
+		Order order = new Order(1, new StorageShelf[] { shelf });
+		assertFalse(order.processItem(new OrderItem(shelfTwo, 1)));
+		assertEquals(1, order.outstandingItemsSize());
+		assertEquals(0, order.processedItemsSize());
 	}
 }
--- src/model/Order.java
+++ src/model/Order.java
@@ -18,57 +18,126 @@ public class Order {
 	/**
 	 * Creates a new Order
 	 * 
-	 * @param shelves The location of each item in this Order
+	 * @param ticksToPack The number of ticks this Order will take to pack
+	 * @param shelves     The location of each item in this Order
 	 */
 	public Order(int ticksToPack, StorageShelf[] shelves) {
 		this.ticksToPack = ticksToPack;
-		for(StorageShelf shelf : shelves) {
+		for (StorageShelf shelf : shelves) {
 			addItem(shelf);
 		}
 	}
 
+	/**
+	 * Adds a new item to the order
+	 * 
+	 * @param shelf The item to add to the order
+	 */
 	public void addItem(StorageShelf shelf) {
-		if(outstandingItems.contains(new OrderItem(shelf, 0))) {
-			outstandingItems.get(outstandingItems.indexOf(new OrderItem(shelf, 0))).incrementQty();
+		if (outstandingItems.contains(new OrderItem(shelf, 0))) {
+			int index = outstandingItems.indexOf(new OrderItem(shelf, 0));
+			outstandingItems.get(index).incrementQty();
 		} else {
 			outstandingItems.add(new OrderItem(shelf, 1));
 		}
 	}
-	
-	public void decrementTicksToPack() {
-		ticksToPack--;
-	}
-	
-	public void incrementTimeProcessing() {
-		timeProcessing++;
-	}
-	
-	public boolean isPacked() {
-		return ticksToPack ==  0;
+
+	/**
+	 * Decrements to ticks to pack by one
+	 * 
+	 * @return Was this decremented successfully
+	 */
+	public boolean decrementTicksToPack() {
+		if (ticksToPack > 0) {
+			ticksToPack--;
+			return true;
+		}
+		return false;
 	}
 
 	/**
-	 * Gets the items on this order
+	 * Gets the next item on this order
 	 * 
-	 * @return The item locations for this order
+	 * @return The next item outstanding, will be null if no items
 	 */
 	public OrderItem getNextItem() {
-		if(!outstandingItems.isEmpty()) {
+		if (!outstandingItems.isEmpty()) {
 			return outstandingItems.get(0);
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Gets to ticks to pack the order
+	 * 
+	 * @return the ticks required to pack the order
+	 */
+	public int getTicksToPack() {
+		return ticksToPack;
+	}
+
+	/**
+	 * Gets the time this order has been processed for
+	 * 
+	 * @return the ticks spent on this order
+	 */
 	public int getTimeProcessing() {
 		return timeProcessing;
 	}
-	
-	public void processItem(OrderItem item) {
-		outstandingItems.remove(item);
-		processedItems.add(item);
+
+	/**
+	 * Increments the tick spent processing this order by one tick
+	 */
+	public void incrementTimeProcessing() {
+		timeProcessing++;
 	}
-	
+
+	/**
+	 * Gets if there are any outstanding items
+	 * 
+	 * @return if the order has been completed
+	 */
 	public boolean isComplete() {
 		return outstandingItems.isEmpty();
+	}
+
+	/**
+	 * Gets if the order has any time remaining for packing
+	 * 
+	 * @return if the order has been packed
+	 */
+	public boolean isPacked() {
+		return ticksToPack == 0;
+	}
+
+	/**
+	 * Gets the number of outstanding unique items on the order
+	 * 
+	 * @return the number of outstanding items (unique)
+	 */
+	public int outstandingItemsSize() {
+		return outstandingItems.size();
+	}
+
+	/**
+	 * Processes an item by moving it from outstanding to processed
+	 * 
+	 * @param item The item to process
+	 * @return if the item was successfully processed
+	 */
+	public boolean processItem(OrderItem item) {
+		if (outstandingItems.remove(item)) {
+			return processedItems.add(item);
+		}
+		return false;
+	}
+
+	/**
+	 * Gets the number of processed unique items on the order
+	 * 
+	 * @return the number of processed items (unique)
+	 */
+	public int processedItemsSize() {
+		return processedItems.size();
 	}
 }
