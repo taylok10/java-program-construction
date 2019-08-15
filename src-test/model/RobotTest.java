4fc5fc5706774ad017498c0e4af398efb528cba9
Kelly Taylor <taylok10@aston.ac.uk>
2019-08-15 21:42:13 +0100

    Robot bug fixes & unit test progress

--- src-test/model/RobotTest.java
+++ src-test/model/RobotTest.java
@@ -3,9 +3,12 @@
  */
 package model;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,16 +19,12 @@ import org.junit.Test;
  */
 public class RobotTest {
 	private static final String IDENTIFIER = "r";
-	private static final Cell LOCATION_ONE = new Cell(0,0);
-	private static final Cell LOCATION_TWO = new Cell(0,1);
 	private Robot robotOne;
-	private ChargingPod chargingPod = new ChargingPod(LOCATION_ONE, 1);
-	private ChargingPod chargingPodTwo = new ChargingPod(LOCATION_TWO, 1);
-	private PathFinder pathFinder = new BFS(new Cell[4][4]);
+	//private PathFinder pathFinder = new BFS(new Cell[4][4]);
 
 	@Before
 	public void setup() {
-		robotOne = new Robot(LOCATION_ONE, 1, chargingPod, pathFinder);
+		robotOne = new Robot(MockWarehouse.LOCATION_ONE, 20, MockWarehouse.CHARGING_POD, MockWarehouse.PATH_FINDER);
 	}
 
 	@Test
@@ -36,12 +35,154 @@ public class RobotTest {
 	@Test
 	public void testNewRobotIncrementsUID() {
 		int currentUID = Integer.parseInt(robotOne.getUID().replaceFirst(IDENTIFIER, ""));
-		Robot robotTwo = new Robot(LOCATION_TWO, 1, chargingPodTwo, pathFinder);
+		Robot robotTwo = new Robot(MockWarehouse.LOCATION_TWO, 1, MockWarehouse.CHARGING_POD, MockWarehouse.PATH_FINDER);
 		assertEquals(IDENTIFIER + ++currentUID, robotTwo.getUID());
 	}
 
 	@Test
 	public void testGetPosition() {
-		assertEquals(LOCATION_ONE, robotOne.getPosition());
+		assertEquals(MockWarehouse.LOCATION_ONE, robotOne.getPosition());
+	}
+	
+	@Test
+	public void testResetId() {
+		Robot.resetIdCount();
+		Robot robotTwo = new Robot(MockWarehouse.LOCATION_TWO, 1, MockWarehouse.CHARGING_POD, MockWarehouse.PATH_FINDER);
+		assertEquals("0", robotTwo.getUID().replaceFirst(IDENTIFIER, ""));
+	}
+	
+	@Test
+	public void testChargeBattery() {
+		robotOne.setBattery(1);
+		robotOne.setRobotState(RobotState.CHARGING);
+		robotOne.chargeBattery(1);
+		assertEquals(2, robotOne.getBattery());
+	}
+	
+	@Test
+	public void testChargeBatteryMultipleIncrement() {
+		robotOne.setBattery(1);
+		robotOne.setRobotState(RobotState.CHARGING);
+		robotOne.chargeBattery(5);
+		assertEquals(6, robotOne.getBattery());
+	}
+	
+	@Test
+	public void testChargeBatteryWhenBatteryFull() {
+		robotOne.setRobotState(RobotState.CHARGING);
+		robotOne.chargeBattery(5);
+		assertEquals(20, robotOne.getBattery());
+	}
+	
+	@Test
+	public void testChargeBatteryWhenNotCharging() {
+		robotOne.setBattery(1);
+		robotOne.chargeBattery(1);
+		assertEquals(1, robotOne.getBattery());
+	}
+	
+	@Test
+	public void testCreateRobot() {
+		int battery = 20;
+		
+		Robot robotTwo = new Robot(MockWarehouse.LOCATION_TWO, battery, MockWarehouse.CHARGING_POD, MockWarehouse.PATH_FINDER);
+		assertEquals(battery, robotTwo.getBattery());
+		assertEquals(battery, robotTwo.getMaxBattery());
+		assertEquals(RobotState.IDLE, robotTwo.getState());
+		assertEquals(MockWarehouse.CHARGING_POD, robotTwo.getChargingPod());
+		assertEquals(MockWarehouse.PATH_FINDER, robotTwo.getPathFinder());
+	}
+	
+	@Test
+	public void testDeliverItemWhenNoCurrentRequest() {
+		assertFalse(robotOne.deliverItemToPackingStation());
+	}
+	
+	@Test
+	public void testDeliverItemToPackingStation() {
+		OrderManager orders = new OrderManager();
+		Order orderOne = new Order(1, new StorageShelf[] { MockWarehouse.SHELF_ONE });
+		orders.addOrder(orderOne);
+		ArrayList<Robot> robots = new ArrayList<Robot>();
+		robots.add(robotOne);
+		PackingStation packingStation = new PackingStation(MockWarehouse.LOCATION_ONE, orders, robots);
+		packingStation.act();
+		assertTrue(robotOne.deliverItemToPackingStation());
+		assertNull(robotOne.getCurrentAssignment());
+	}
+	
+	@Test
+	public void testDeliverItemRejectedByPackingStation() {
+		OrderManager orders = new OrderManager();
+		Order orderOne = new Order(1, new StorageShelf[] { MockWarehouse.SHELF_ONE });
+		orders.addOrder(orderOne);
+		ArrayList<Robot> robots = new ArrayList<Robot>();
+		robots.add(robotOne);
+		PackingStation packingStation = new PackingStation(MockWarehouse.LOCATION_ONE, orders, robots);
+		packingStation.act();
+		robotOne.setCurrentAssignment(new Assignment(packingStation, MockWarehouse.SHELF_TWO));
+		assertFalse(robotOne.deliverItemToPackingStation());
+	}
+	
+	@Test
+	public void testAssignmentRequest() {
+		Assignment assignment = new Assignment(MockWarehouse.PACKING_STATION, MockWarehouse.SHELF_TWO);
+		assertTrue(robotOne.assignmentRequest(assignment));
+		assertEquals(assignment, robotOne.getCurrentAssignment());
+	}
+	
+	@Test
+	public void testAssignmentRequestWhileCharging() {
+		Assignment assignment = new Assignment(MockWarehouse.PACKING_STATION, MockWarehouse.SHELF_TWO);
+		robotOne.setRobotState(RobotState.CHARGING);
+		assertFalse(robotOne.assignmentRequest(assignment));
+		assertNull(robotOne.getCurrentAssignment());
+	}
+	
+	@Test
+	public void testAssignmentRequestWhileHasRequest() {
+		Assignment assignmentOne = new Assignment(MockWarehouse.PACKING_STATION, MockWarehouse.SHELF_TWO);
+		Assignment assignmentTwo = new Assignment(MockWarehouse.PACKING_STATION, MockWarehouse.SHELF_THREE);
+		robotOne.setCurrentAssignment(assignmentOne);
+		assertFalse(robotOne.assignmentRequest(assignmentTwo));
+		assertEquals(assignmentOne, robotOne.getCurrentAssignment());
+	}
+	
+	@Test
+	public void testAssignmentRequestWhileCannotHandleRequest() {
+		robotOne.setBattery(1);
+		Assignment assignment = new Assignment(MockWarehouse.PACKING_STATION, MockWarehouse.SHELF_FOUR);
+		assertFalse(robotOne.assignmentRequest(assignment));
+		assertNull(robotOne.getCurrentAssignment());
+	}
+	
+	@Test
+	public void testActIdle() {
+		assertTrue(robotOne.act());
+		assertEquals(RobotState.IDLE, robotOne.getState());
+	}
+	
+	@Test
+	public void testActCharging() {
+		robotOne.setBattery(1);
+		robotOne.setRobotState(RobotState.CHARGING);
+		assertTrue(robotOne.act());
+		assertEquals(RobotState.CHARGING, robotOne.getState());
+	}
+	
+	@Test
+	public void testActChargingBatteryFull() {
+		robotOne.setRobotState(RobotState.CHARGING);
+		assertTrue(robotOne.act());
+		assertEquals(RobotState.IDLE, robotOne.getState());
+	}
+	
+	@Test
+	public void testActChargingBatteryFullWithEmergencyState() {
+		robotOne.setRobotState(RobotState.CHARGING);
+		robotOne.setEmergencyState(RobotState.DELIVERING_ITEM);
+		assertTrue(robotOne.act());
+		assertEquals(RobotState.DELIVERING_ITEM, robotOne.getState());
+		assertNull(robotOne.getEmergencyBackupState());
 	}
 }
--- src/model/Robot.java
+++ src/model/Robot.java
@@ -1,8 +1,12 @@
 package model;
 
+/**
+ * @author kelly.taylor
+ *
+ */
 public class Robot extends Actor {
 	private static final String IDENTIFIER = "r";
-	private static final FuelCalculator FUEL_CALCULATOR = new FuelCalculator(1,2);
+	private static final FuelCalculator FUEL_CALCULATOR = new FuelCalculator(1, 2);
 	private static int id = 0;
 
 	private int battery;
@@ -13,7 +17,16 @@ public class Robot extends Actor {
 	private PathFinder<GridLocation> pathFinder;
 	private boolean hasItem;
 	private RobotState emergencyBackupState = null;
+	private int deadlockCounter = 0;
 
+	/**
+	 * Creates a new Robot
+	 * 
+	 * @param position    The position of this PackingStation
+	 * @param maxBattery  The maximum battery of the Robot
+	 * @param chargingPod The chargingPod assigned to this Robot
+	 * @param pathFinder  The pathFinder used for navigating the warehouse
+	 */
 	public Robot(GridLocation position, int maxBattery, ChargingPod chargingPod, PathFinder<GridLocation> pathFinder) {
 		super(position, IDENTIFIER + id);
 		id++;
@@ -23,51 +36,74 @@ public class Robot extends Actor {
 		this.pathFinder = pathFinder;
 	}
 
-	// Getter methods
-	public ChargingPod getChargingPod() {
-		return chargingPod;
-	}
-	
-	public void chargeBattery(int charge) {
-		if(state == RobotState.CHARGING) {
-			battery += charge;
-			if(battery > maxBattery) {
-				battery = maxBattery;
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see model.Actor#act()
+	 */
+	@Override
+	public boolean act() {
+		boolean flag = true;
+		switch (state) {
+		case COLLECTING_ITEM:
+			flag = moveTowards(currentAssignment.getShelf().getPosition());
+			if (getPosition().equals(currentAssignment.getShelf().getPosition())) {
+				// Collected item, deliver it to packing station
+				hasItem = true;
+				state = RobotState.DELIVERING_ITEM;
 			}
+			break;
+		case DELIVERING_ITEM:
+			flag = moveTowards(currentAssignment.getPackingStation().getPosition());
+			if (getPosition().equals(currentAssignment.getPackingStation().getPosition())) {
+				// Delivered item return to pod
+				if (deliverItemToPackingStation()) {
+					state = RobotState.RETURNING_TO_POD;
+					currentAssignment = null;
+					hasItem = false;
+				}
+			}
+			break;
+		case RETURNING_TO_POD:
+			flag = moveTowards(chargingPod.getPosition());
+			if (getPosition().equals(chargingPod.getPosition())) {
+				// Reached the charging pod, now we can charge
+				if (chargingPod.dockRobot(this)) {
+					state = RobotState.CHARGING;
+				}
+			}
+			break;
+		case CHARGING:
+			if (battery == maxBattery) { // Stop charging when full
+				chargingPod.undockRobot(); // Detach from pod
+
+				if (emergencyBackupState != null) {
+					// The Robot had to make an emergency re-route, pick up where we were
+					state = emergencyBackupState;
+					emergencyBackupState = null;
+				} else {
+					state = RobotState.IDLE;
+				}
+			}
+			break;
+		case IDLE:
+		default:
+			// Do nothing
+			break;
 		}
+		return flag;
 	}
 
-	private boolean canHandleAssignment(Assignment assignment) {
-		int cost = 0;
-		
-		// Calculate journey from location to shelf to collect item
-		PathLink<GridLocation> pathToShelf = pathFinder.findPath(getPosition(), assignment.getShelf().getPosition(), true);
-		if(pathToShelf == null) {
-			return false;
-		}
-		cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToShelf.size(), false);
-		
-		// Calculate journey from shelf to PackingStation
-		PathLink<GridLocation> pathToPackingStation = pathFinder.findPath(assignment.getShelf().getPosition(), assignment.getPackingStation().getPosition(), true);
-		if(pathToPackingStation == null) {
-			return false;
-		}
-		cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPackingStation.size(), true);
-		
-		// Need to know the charging pod location for returning to
-		PathLink<GridLocation> pathToPod = pathFinder.findPath(assignment.getPackingStation().getPosition(), chargingPod.getPosition(), true);
-		if(pathToPod == null) {
-			return false;
-		}
-		cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPod.size(), false);
-		
-		// If the cost is less than our battery we can accept
-		return cost < battery;
-	}
-	
+	/**
+	 * Asks the robot if they can accept the provided assignment
+	 * 
+	 * @param assignment The assignment for the Robot
+	 * @return If the robot has accepted
+	 */
 	public boolean assignmentRequest(Assignment assignment) {
-		if(currentAssignment == null && state != RobotState.CHARGING && canHandleAssignment(assignment)) { // If Robot is not doing anything/not on an order
-			//Update state if accepted & assignment
+		// If Robot is not doing anything/not on an order
+		if (currentAssignment == null && state != RobotState.CHARGING && canHandleAssignment(assignment)) {
+			// Update state if accepted & assignment
 			state = RobotState.COLLECTING_ITEM;
 			currentAssignment = assignment;
 			return true;
@@ -75,15 +111,22 @@ public class Robot extends Actor {
 		return false;
 	}
 
+	/**
+	 * Attempts to move the robot to the specified location
+	 * 
+	 * @param location The location to move to
+	 * @return If the robot can continue
+	 */
 	private boolean attemptToMove(GridLocation location) {
-		if(battery == 0) {
-			WarehouseSimulation.setFailureReason("ERROR - Robot: " + getUID() + " has run out of battery at " + WarehouseSimulation.getTicks() + " ticks");
+		if (battery == 0) {
+			WarehouseSimulation.setFailureReason("ERROR - Robot: " + getUID() + " has run out of battery at "
+					+ WarehouseSimulation.getTicks() + " ticks");
 			WarehouseSimulation.finish(false);
 			return false;
 		}
-			
-		if(location != null) {
-			if(move(location)) {
+
+		if (location != null) {
+			if (move(location)) {
 				// Reduce battery
 				battery -= FUEL_CALCULATOR.calculateFuelConsumption(1, hasItem);
 			}
@@ -91,148 +134,283 @@ public class Robot extends Actor {
 		return true;
 	}
 
+	/**
+	 * Advises if the Robot can handle the provided assignment by reviewing the cost
+	 * of the journey to the current battery
+	 * 
+	 * @param assignment The assignment to attempt
+	 * @return If the Robot can handle the assignment
+	 */
+	private boolean canHandleAssignment(Assignment assignment) {
+		int cost = 0;
+
+		// Calculate journey from location to shelf to collect item
+		PathLink<GridLocation> pathToShelf = pathFinder.findPath(getPosition(), assignment.getShelf().getPosition(),
+				false);
+		if (pathToShelf == null) {
+			return false;
+		}
+		cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToShelf.size(), false);
+
+		// Calculate journey from shelf to PackingStation
+		PathLink<GridLocation> pathToPackingStation = pathFinder.findPath(assignment.getShelf().getPosition(),
+				assignment.getPackingStation().getPosition(), true);
+		if (pathToPackingStation == null) {
+			return false;
+		}
+		cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPackingStation.size(), true);
+
+		// Need to know the charging pod location for returning to
+		PathLink<GridLocation> pathToPod = pathFinder.findPath(assignment.getPackingStation().getPosition(),
+				chargingPod.getPosition(), true);
+		if (pathToPod == null) {
+			return false;
+		}
+		cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPod.size(), false);
+
+		// If we have the battery we can accept
+		return battery > cost;
+	}
+
+	/**
+	 * Advises if the Robot can return to the pod in their current condition
+	 * 
+	 * @return If Robot can return to the ChargingPod
+	 */
+	private boolean canReturnToPod() {
+		PathLink<GridLocation> pathToPod = pathFinder.findPath(getPosition(), chargingPod.getPosition(), false);
+		if (pathToPod != null) {
+			return battery > FUEL_CALCULATOR.calculateFuelConsumption(pathToPod.size(), hasItem);
+		}
+		return false;
+	}
+
+	/**
+	 * Charges the battery of the robot. Required the Robot to be in Charging state.
+	 * 
+	 * @param charge The amount to charge the battery by
+	 */
+	public void chargeBattery(int charge) {
+		if (state == RobotState.CHARGING) {
+			battery += charge;
+			if (battery > maxBattery) {
+				battery = maxBattery;
+			}
+		}
+	}
+
+	/**
+	 * Hands over the current assignment to the packing station
+	 * 
+	 * @return If the assignment was delivered successfully
+	 */
 	public boolean deliverItemToPackingStation() {
-		if(currentAssignment.getPackingStation().acceptItemDelivery(currentAssignment.getShelf())) {
+		if (currentAssignment != null
+				&& currentAssignment.getPackingStation().acceptItemDelivery(currentAssignment.getShelf())) {
 			currentAssignment = null;
 			return true;
 		}
 		return false;
 	}
 
-	public static void resetIdCount() {
-        id = 0;
-    }
+	/**
+	 * Gets the current battery level of the robot
+	 * 
+	 * @return Battery level
+	 */
+	public int getBattery() {
+		return battery;
+	}
 
+	/**
+	 * Gets the assigned ChargingPod of the robot
+	 * 
+	 * @return Assigned ChargingPod
+	 */
+	public ChargingPod getChargingPod() {
+		return chargingPod;
+	}
+
+	/**
+	 * Gets the current assignment of the robot
+	 * 
+	 * @return Current assignment
+	 */
+	public Assignment getCurrentAssignment() {
+		return currentAssignment;
+	}
+
+	/**
+	 * Gets the backed up state of the robot before it entered emergency mode
+	 * 
+	 * @return Backed up state
+	 */
+	public RobotState getEmergencyBackupState() {
+		return emergencyBackupState;
+	}
+
+	/**
+	 * Gets the maximum battery level of the robot
+	 * 
+	 * @return Max battery level
+	 */
+	public int getMaxBattery() {
+		return maxBattery;
+	}
+
+	/**
+	 * Gets the path finder of the robot
+	 * 
+	 * @return The path finder being used for navigation
+	 */
+	public PathFinder<GridLocation> getPathFinder() {
+		return pathFinder;
+	}
+
+	/**
+	 * Gets the current state of the robot
+	 * 
+	 * @return Robot state
+	 */
+	public RobotState getState() {
+		return state;
+	}
+
+	/**
+	 * Will try to move the Robot towards the specified target. If there is not
+	 * enough battery they may enter an emergency state and return to their pod.
+	 * 
+	 * @param location The location to move towards
+	 * @return If the robot is still active
+	 */
 	private boolean moveTowards(GridLocation location) {
-		if(!canFinishJourney() && emergencyBackupState == null) {
-			emergencyBackupState = state;
-			state = RobotState.RETURNING_TO_POD;
-			location = chargingPod.getPosition();
-		}
-		
-		PathLink<GridLocation> path = pathFinder.findPath(getPosition(), location, false);
-		if (path != null) {
-			return attemptToMove(path.takeStep());
+		// Check if the Robot has ended up in a deadlock state with another Robot
+		// This happens if they want each other's spaces
+		boolean inDeadlockState = deadlockCounter >= 5;
+		if (emergencyBackupState != null && !canFinishJourney()) {
+			if (canReturnToPod()) {
+				emergencyBackupState = state;
+				state = RobotState.RETURNING_TO_POD;
+				location = chargingPod.getPosition();
+			}
+		} else if (canFinishJourney() || inDeadlockState) {
+			PathLink<GridLocation> path = pathFinder.findPath(getPosition(), location, inDeadlockState);
+			if (path != null) {
+				return attemptToMove(path.takeStep());
+			}
 		}
 		return true;
 	}
-	
+
+	/**
+	 * Resets the id seed of Robots
+	 */
+	public static void resetIdCount() {
+		id = 0;
+	}
+
+	/**
+	 * Sets the current battery level of the robot
+	 * 
+	 * @param battery New battery value
+	 */
+	public void setBattery(int battery) {
+		this.battery = battery;
+	}
+
+	/**
+	 * Sets the current assignment of the robot
+	 * 
+	 * @param assignment New assignment
+	 */
+	public void setCurrentAssignment(Assignment assignment) {
+		this.currentAssignment = assignment;
+	}
+
+	/**
+	 * Sets the state of the robot
+	 * 
+	 * @param state The new state of the Robot
+	 */
+	public void setEmergencyState(RobotState state) {
+		this.emergencyBackupState = state;
+	}
+
+	/**
+	 * Sets the state of the robot
+	 * 
+	 * @param state New state
+	 */
+	public void setRobotState(RobotState state) {
+		this.state = state;
+	}
+
+	// TODO: Refactor below method
+
 	private boolean canFinishJourney() {
 		int cost = 0;
-		if(state == RobotState.COLLECTING_ITEM) {
-			PathLink<GridLocation> pathToShelf = pathFinder.findPath(getPosition(), currentAssignment.getShelf().getPosition(), false);
-			if(pathToShelf == null) {
-				// No available path currently - assume they can still finish the journey as no battery change
+		if (state == RobotState.COLLECTING_ITEM) {
+			PathLink<GridLocation> pathToShelf = pathFinder.findPath(getPosition(),
+					currentAssignment.getShelf().getPosition(), false);
+			if (pathToShelf == null) {
+				// No available path currently - assume they can still finish the journey as no
+				// battery change
+				deadlockCounter++;
 				return true;
 			}
 			cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToShelf.size(), false);
-			
-			PathLink<GridLocation> pathToPackingStation = pathFinder.findPath(currentAssignment.getShelf().getPosition(), currentAssignment.getPackingStation().getPosition(), true);
-			if(pathToPackingStation == null) {
-				// No available path currently - assume they can still finish the journey as no battery change
+
+			PathLink<GridLocation> pathToPackingStation = pathFinder.findPath(
+					currentAssignment.getShelf().getPosition(), currentAssignment.getPackingStation().getPosition(),
+					true);
+			if (pathToPackingStation == null) {
+				// No available path currently - assume they can still finish the journey as no
+				// battery change
+				deadlockCounter++;
 				return true;
 			}
 			cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPackingStation.size(), true);
-			
+
 			PathLink<GridLocation> pathToPod = pathFinder.findPath(getPosition(), chargingPod.getPosition(), true);
-			if(pathToPod == null) {
-				// No available path currently - assume they can still finish the journey as no battery change
+			if (pathToPod == null) {
+				// No available path currently - assume they can still finish the journey as no
+				// battery change
+				deadlockCounter++;
 				return true;
 			}
+
 			cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPod.size(), false);
 		}
 		if (state == RobotState.DELIVERING_ITEM) {
-			PathLink<GridLocation> pathToPackingStation = pathFinder.findPath(getPosition(), currentAssignment.getPackingStation().getPosition(), false);
-			if(pathToPackingStation == null) {
-				// No available path currently - assume they can still finish the journey as no battery change
+			PathLink<GridLocation> pathToPackingStation = pathFinder.findPath(getPosition(),
+					currentAssignment.getPackingStation().getPosition(), false);
+			if (pathToPackingStation == null) {
+				// No available path currently - assume they can still finish the journey as no
+				// battery change
+				deadlockCounter++;
 				return true;
 			}
 			cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPackingStation.size(), true);
-			
+
 			PathLink<GridLocation> pathToPod = pathFinder.findPath(getPosition(), chargingPod.getPosition(), true);
-			if(pathToPod == null) {
-				// No available path currently - assume they can still finish the journey as no battery change
+			if (pathToPod == null) {
+				// No available path currently - assume they can still finish the journey as no
+				// battery change
+				deadlockCounter++;
 				return true;
 			}
 			cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPod.size(), false);
 		}
 		if (state == RobotState.RETURNING_TO_POD) {
 			PathLink<GridLocation> pathToPod = pathFinder.findPath(getPosition(), chargingPod.getPosition(), false);
-			if(pathToPod == null) {
-				// No available path currently - assume they can still finish the journey as no battery change
+			if (pathToPod == null) {
+				// No available path currently - assume they can still finish the journey as no
+				// battery change
+				deadlockCounter++;
 				return true;
 			}
 			cost += FUEL_CALCULATOR.calculateFuelConsumption(pathToPod.size(), false);
 		}
-		return battery > cost;
-	}
-
-	@Override
-	public boolean act() {
-//		WarehouseSimulation.addReportEntry("ROBOT - " + getUID() + ": " + state + " | " + battery + "Units | " + getPosition().getRow() + "," + getPosition().getColumn());
-		boolean flag = true;
-		switch(state) {
-			case COLLECTING_ITEM:
-				flag = moveTowards(currentAssignment.getShelf().getPosition());
-				if(getPosition().equals(currentAssignment.getShelf().getPosition())) {
-					// Collected item, deliver it to packing station
-					hasItem = true;
-					state = RobotState.DELIVERING_ITEM;
-				}
-				break;
-			case DELIVERING_ITEM:
-				flag = moveTowards(currentAssignment.getPackingStation().getPosition());
-				if(getPosition().equals(currentAssignment.getPackingStation().getPosition())) {
-					// Delivered item return to pod
-					if(deliverItemToPackingStation()) {
-						state = RobotState.RETURNING_TO_POD;
-						currentAssignment = null;
-						hasItem = false;
-					}
-				}
-				break;
-			case RETURNING_TO_POD:
-				flag = moveTowards(chargingPod.getPosition());
-				if(getPosition().equals(chargingPod.getPosition())) {
-					// Reached the charging pod, now we can charge
-					if(chargingPod.dockRobot(this)) {
-						state = RobotState.CHARGING;
-					}
-				}
-				break;
-			case CHARGING:
-				if(battery == maxBattery) { // Stop charging when full
-					chargingPod.undockRobot(); // Detach from pod
-					
-					if(emergencyBackupState != null) { // The Robot had to make an emergency re-route, pick up where we were
-						state = emergencyBackupState;
-						emergencyBackupState = null;
-					} else {
-						state = RobotState.IDLE;
-					}
-				}
-				break;
-			case IDLE:
-			default:
-				// Do nothing
-				break;
-		}
-		return flag;
-	}
-	
-	public RobotState getState() {
-		return state;
-	}
-	
-	public int getBattery() {
-		return battery;
-	}
-	
-	public void setBattery(int battery) {
-		this.battery = battery;
-	}
-	
-	public void setRobotState(RobotState state) {
-		this.state = state;
+		return battery >= cost;
 	}
 }
